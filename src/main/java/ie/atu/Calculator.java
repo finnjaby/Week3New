bf9cd62e550ef9bf69976078b185fea5642df92e
Finnja <g00470359@atu.ie>
2025-10-01 17:29:55 +0100

    added multiply

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
         return firstNum + secondNum;
     }
     public int sub(int firstNum, int secondNum) {
+
         return firstNum - secondNum;
+    }
+    public int mul(int firstNum, int secondNum) {
+        return firstNum * secondNum;
     }
 }
